ca047ae213232789068f8b45bfde9e9f544183b6
Syed Faizan Mehdi Zaidi <Sfmzaidi@myseneca.ca>
2024-09-24 06:11:28 +0500

    Trigger redeploy with minor changes

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
+// Minor change for redeploy
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Middleware to log requests
-app.use((req, res, next) => {
-    console.log(`Request URL: ${req.url}`);
-    next();
-});
 
 app.get('/', (req, res) => {
     res.send('Syed Faizan Mehdi Zaidi - 136151230');
