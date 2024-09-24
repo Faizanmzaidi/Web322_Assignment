d95f106a1df492861ffae79fecdebd785cb30443
Syed Faizan Mehdi Zaidi <Sfmzaidi@myseneca.ca>
2024-09-24 06:14:17 +0500

    Small change to trigger redeploy

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-// Minor change for redeploy
+// Redeploy trigger change
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Syed Faizan Mehdi Zaidi - 136151230');
+  res.send('Syed Faizan Mehdi Zaidi - 136151230');
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
