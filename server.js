9358aac9a6016515283623ee6236d15342569d43
Syed Faizan Mehdi Zaidi <Sfmzaidi@myseneca.ca>
2024-09-24 06:09:03 +0500

    Added logging for requests

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware to log requests
+app.use((req, res, next) => {
+    console.log(`Request URL: ${req.url}`);
+    next();
+});
+
 app.get('/', (req, res) => {
-  res.send('Syed Faizan Mehdi Zaidi - 136151230');
+    res.send('Syed Faizan Mehdi Zaidi - 136151230');
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
